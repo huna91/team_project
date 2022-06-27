1a91cb6575691254b9a5f260f2d38055b3f2e417
WooJonghwa <wjh423@gmail.com>
2022-06-27 17:54:34 +0900

    수업끝

--- JH_page/JS/JH.js
+++ JH_page/JS/JH.js
@@ -26,3 +26,8 @@ function ve_goNext() {
     behavior: 'smooth' 
   });
 }
+// 참고링크
+// https://junjangsee.tistory.com/entry/CSS-scroll-snap-%EC%86%8D%EC%84%B1%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%98%EC%97%AC-%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
+// https://stackoverflow.com/questions/57518428/css-scroll-snap-points-with-navigation-next-previous-buttons
+
+
