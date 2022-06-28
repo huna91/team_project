186f053caae4ef1fd1d78749f502550d51b87932
WooJonghwa <wjh423@gmail.com>
2022-06-28 12:48:38 +0900

    오전

--- JH_page/JS/JH.js
+++ JH_page/JS/JH.js
@@ -1,5 +1,5 @@
 
-// 클릭하면 스크롤스냅 //
+// 버튼 클릭으로 스크롤스냅 //
 
 function cu_goPrecious() {
   document.querySelector('.curated_slider').scrollBy({ 
@@ -31,3 +31,43 @@ function ve_goNext() {
 // https://stackoverflow.com/questions/57518428/css-scroll-snap-points-with-navigation-next-previous-buttons
 
 
+
+
+// 스크롤 하면 이미지 고정되고 효과걸리게 //
+
+let stop_bg = document.querySelector(".stop_img");
+let _top = stop_bg.getBoundingClientRect().top - window.pageYOffset;
+/*
+window.onscroll = function(){
+  if (window.scrollY > _top) {
+    if (!stop_bg.classList.contains('stop_')) {
+      stop_bg.classList.add("stop_");
+
+    }
+  }
+  else {
+    stop_bg.classList.remove("stop_")
+  }
+
+}*/
+
+  /*
+  console.log("_top = "+ _top)
+  console.log("window.pageYOffset = "+ window.pageYOffset);
+  console.log("window.scrollY = "+ window.scrollY);
+  console.log(stop_bg.getBoundingClientRect().top);
+  console.log(stop_bg.classList);
+  */
+
+
+window.addEventListener("scroll", function(){
+  if (window.scrollY > _top) {
+    if (!stop_bg.classList.contains("stop_")) {
+      stop_bg.classList.add("stop_");
+    }
+  }
+  else {
+    stop_bg.classList.remove("stop_");
+  }
+
+})
